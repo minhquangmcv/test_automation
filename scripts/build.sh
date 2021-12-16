0519bb4770b508ece75af29f4948c5bdf9112a4f
DESKTOP-B2MPR32\minhq <minhquang130698@gmail.com>
2021-12-16 14:22:24 +0700

    ! update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,25 +12,30 @@ git clean -fd
 git pull
 echo
 echo "----------------- END CLEAN UP & UPDATE WORKSPACE -----------------"
+echo
 
 echo
 echo "================= UPGRADE VERSION ================="
+echo
 cd ./test_automation
 melos.bat version rong_vang_dev $RONG_VANG_VERSION --yes --no-changelog
 melos.bat version rong_vang_prod $RONG_VANG_VERSION --yes --no-changelog
 echo
 echo "----------------- END UPGRADE VERSION -----------------"
+echo
 
 echo
 echo "================= COMMIT CHANGES ================="
+echo
 git add ../scripts/config.sh
 git add ../app/rong_vang_dev/pubspec.yaml
 git add ../app/rong_vang_prod/pubspec.yaml
 
-#git commit -m "Upgrade version to $RONG_VANG_VERSION"
+git commit -m "Upgrade version to $RONG_VANG_VERSION"
 
 git push
 
 git status
 echo
 echo "----------------- END COMMIT CHANGES -----------------"
+echo
